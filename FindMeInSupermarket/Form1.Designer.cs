b3ccb62f159e3f7b0a76f5e0e8e52aa7396ab092
MauraYufi <maurayufi@mail.ugm.ac.id>
2021-04-29 00:31:35 +0700

    First Commit

---
+++ FindMeInSupermarket/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace FindMeInSupermarket
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbKeywoard = new System.Windows.Forms.TextBox();
+            this.btnCari = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pencarian :";
+            // 
+            // tbKeywoard
+            // 
+            this.tbKeywoard.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.tbKeywoard.Location = new System.Drawing.Point(149, 25);
+            this.tbKeywoard.Name = "tbKeywoard";
+            this.tbKeywoard.Size = new System.Drawing.Size(697, 26);
+            this.tbKeywoard.TabIndex = 1;
+            this.tbKeywoard.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbKeywoard_KeyPress);
+            // 
+            // btnCari
+            // 
+            this.btnCari.Location = new System.Drawing.Point(863, 21);
+            this.btnCari.Name = "btnCari";
+            this.btnCari.Size = new System.Drawing.Size(181, 35);
+            this.btnCari.TabIndex = 2;
+            this.btnCari.Text = "Cari";
+            this.btnCari.UseVisualStyleBackColor = true;
+            this.btnCari.Click += new System.EventHandler(this.btnCari_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 79);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1032, 359);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1056, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnCari);
+            this.Controls.Add(this.tbKeywoard);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Find Me ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbKeywoard;
+        private System.Windows.Forms.Button btnCari;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ FindMeInSupermarket/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindMeInSupermarket
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void tbKeywoard_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                btnCari.PerformClick();
+            }
+        }
+
+        private void btnCari_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["cn"].ConnectionString))
+                {
+                    if (cn.State == ConnectionState.Closed)
+                        cn.Open();
+                    using (DataTable dt = new DataTable("TB_Produk"))
+                    {
+                        using (SqlCommand cmd = new SqlCommand("select *from TB_Produk where Kategori like @Kategori or Merk like @Merk", cn))
+                        {
+                            cmd.Parameters.AddWithValue("Kategori", string.Format("%{0}%", tbKeywoard.Text));
+                            cmd.Parameters.AddWithValue("Merk", string.Format("%{0}%", tbKeywoard.Text));
+                            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                            adapter.Fill(dt);
+                            dataGridView1.DataSource = dt;
+
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
